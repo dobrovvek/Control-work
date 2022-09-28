8560c07767f034d41d3ca93eebde14e5db25087a
Dobrovvek <dobrovvek@gmail.com>
2022-09-28 16:46:32 +0300

    add Program

---
+++ Program/Program.cs
@@ -0,0 +1,24 @@
+﻿string? stroka;
+string[] Alex;
+
+Console.WriteLine("Введите массив строк через запятую пример - ['0','1', ...] :");
+
+stroka = Console.ReadLine();
+stroka = stroka.Replace("[","");
+stroka = stroka.Replace("]","");
+stroka = stroka.Replace("'","");
+
+Alex = stroka.Split(',');
+
+NewArray(Alex);
+
+void NewArray(string[] array)
+{
+    Console.Write("[ ");
+for(int i = 0; i < array.Length; i++)
+{
+if (array[i].Length > 3 ) i++;
+else Console.Write($"'{array[i]}' ");
+}
+    Console.Write("]");
+}
